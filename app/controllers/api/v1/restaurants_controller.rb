4903a5945519abd8fd6515f973615538ba41e18f
Edward Bent <bentorama@gmail.com>
2021-11-15 16:05:36 +0000

    Finished 4th endpoint - create

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_restaurant, only: [ :show, :update ]
-  
+
   def index
     @restaurants = policy_scope(Restaurant)
   end
@@ -12,6 +12,17 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def update
     if @restaurant.update(restaurant_params)
       render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      render :show, status: :created
     else
       render_error
     end
--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -10,6 +10,12 @@ class RestaurantPolicy < ApplicationPolicy
   end
 
   def update?
+    # only the restaurant owner can update the restaurant
     record.user == user
+  end
+
+  def create?
+    # any logged in user can create a restuarant
+    !user.nil?
   end
 end
